a3d1e63fc89e9d703289a1d8e6ffb0a4a7eb7ce1
Roottoaccess <network123crab@gmail.com>
2025-11-03 12:15:32 +0530

    checked the entrypoint.sh script

--- packages/server/entrypoint.sh
+++ packages/server/entrypoint.sh
@@ -2,24 +2,33 @@
 
 set -ex
 
-dir=$(pwd)
+dir="$(pwd)"
 
 echo "$dir/packages/server/dist/server.js"
 
-# https://docs.docker.com/engine/containers/multi-service_container/
-
 # connect ui
-if [ "$FLAG_SERVE_CONNECT_UI" == "true" ]; then
+if [ "$FLAG_SERVE_CONNECT_UI" = "true" ]; then
   node "$dir/packages/server/dist/server.js" &
+  pid1=$!
 
   # This is not recommended, you should serve Connect UI from a dedicated static website hosting
   npm run -w @nangohq/connect-ui serve:unsafe &
+  pid2=$!
 
-  # Wait for any process to exit
-  wait -n
+  # Wait for either to exit, then kill the other
+  while kill -0 $pid1 2>/dev/null && kill -0 $pid2 2>/dev/null; do
+    sleep 1
+  done
 
-  # Exit with status of process that exited first
-  exit $?
+  # Optionally kill any remaining child (if you want to stop both on one exit)
+  kill $pid1 $pid2 2>/dev/null || true
+
+  # Exit with nonzero if any failed
+  wait $pid1
+  code1=$?
+  wait $pid2
+  code2=$?
+  exit $((code1 != 0 ? code1 : code2))
 else
   node "$dir/packages/server/dist/server.js"
 fi
